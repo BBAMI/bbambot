2b1dec89eb32b0c8665a50cc4a8204cbbd55afb4
BBAMI <48319569+BBAMI@users.noreply.github.com>
2020-07-14 19:03:27 +0900

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -37,7 +37,8 @@ async def on_message(message):
 		choiceresult = choice[choicenumber]
 		await client.send_message(message.channel, str(choiceresult)+'이(가) 좋겠네')
     elif message.content.startswith('뺌 주사위'):
-        await client.send_message(message.channel, '<@'+id+'>의 주사위 : '+random.randint(1,200)+'')
+        dice = random.randint(1,200)
+        await client.send_message(message.channel, '<@'+id+'>의 주사위 : '+dice)
 	elif message.content.startswith('뺌 감타디'):
 		embed = discord.Embed(title="감자타워디펜스",
 		description='다운로드 / 룰북 / 패치노트\nhttps://docs.google.com/spreadsheets/d/1xFTxiUZmJRuARkA7m-pHlYFo97Y0liyCdZbbPofazDM/edit?usp=sharing'
